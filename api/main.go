d9f0fa5e3e9baea405657816fc86d7e5f789ad6e
Paul <paul.junho.yang@gmail.com>
2022-04-22 00:35:25 -0700

    start code

--- api/main.go
+++ api/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"golang.org/x/oauth2"
+)
 
 func main() {
-	fmt.Println("cue api")
+	http.HandleFunc("/user/auth", handleAuth)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleAuth(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+		case "GET":
+			getOAuthToken()
+		case "DELETE":
+			rmOAuthToken()
+	}
+}
+
+var conf = &oauth2.Config{
+	ClientID:     "YOUR_CLIENT_ID",
+	ClientSecret: "YOUR_CLIENT_SECRET",
+	Scopes:       []string{"SCOPE1", "SCOPE2"},
+	Endpoint: oauth2.Endpoint{
+		AuthURL:  "https://provider.com/o/oauth2/auth",
+		TokenURL: "https://provider.com/o/oauth2/token",
+	},
+}
+
+func getOAuthToken() {
+
+}
+
+func rmOAuthToken() {
+
 }
